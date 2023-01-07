f0f08a5b03f7ed165387f0c2e7f44ef796aa748b
Matt <matthewdavid99@comcast.net>
2023-01-07 11:50:39 -0500

    server crashed

--- express101/nodeServer.js
+++ express101/nodeServer.js
@@ -1,12 +1,25 @@
 const http = require('http')
+//
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    // console.log(req);
+    if (req.url === '/') {
 
-    // res = our way of responding to the requester
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>hello matt</h1>')
-    res.end()
+        // console.log(req);
+
+        // res = our way of responding to the requester
+        res.writeHead(200, { 'content-type': 'text/html' })
+        // res.write()
+        const homePageHtml = fs.readFileSync('node.html')
+        res.write(homePageHtml)
+        res.end()
+
+    } else {
+
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.write(`<h3>Sorry this isn't the page you're looking for</h3>`)
+        res.end()
+    }
 })
 
 server.listen(5000)
