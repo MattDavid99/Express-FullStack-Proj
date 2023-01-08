ab068282cdd05f1a76cccab898d20eb2f27f2511
Matt <matthewdavid99@comcast.net>
2023-01-08 13:56:28 -0500

    ejs rendering

--- express2/helmetAndOthers.js
+++ express2/helmetAndOthers.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const app = express()
-const helmet = require('helmet')
+// const express = require('express')
+// const app = express()
+// const helmet = require('helmet')
 
-app.use(helmet())
+// app.use(helmet())
 
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// app.use(express.static('public'))
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
 
-// 1. Static
-// 2. Json
-// 3. urlencoded
+// // 1. Static
+// // 2. Json
+// // 3. urlencoded
 
-app.post('/ajax', (req, res) => {
-    console.log(req.body);
-    res.json('test')
-})
+// app.post('/ajax', (req, res) => {
+//     console.log(req.body);
+//     res.json('test')
+// })
 
-app.listen(5000)
+// app.listen(5000)
