4256e712f264e35bf82c65cc5787fc69cb57d1ea
Matt <matthewdavid99@comcast.net>
2023-01-09 17:08:40 -0500

    remember this cmd, express ejsSite --view=ejs

--- myNewExpressSite/app.js
+++ myNewExpressSite/app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// hello
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
