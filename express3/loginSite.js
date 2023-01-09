b947c62d712d941c0090cef8ba799fd9b0e21fad
Matt <matthewdavid99@comcast.net>
2023-01-09 14:43:43 -0500

    app.param()

--- express3/loginSite.js
+++ express3/loginSite.js
@@ -6,7 +6,9 @@ const app = express()
 const cookieParser = require('cookie-parser')
 
 const helmet = require('helmet')
-const { clear } = require('console')
+const { clear } = require('console') // <<-  idk why this is here, mistake
+const { raw } = require('express')
+const { parseArgs } = require('util')
 app.use(helmet())
 // app.use(helmet({
 //     contentSecurityPolicy: false,
@@ -39,9 +41,6 @@ app.use((req, res, next) => {
     // Send me onto the next piece of middleware
     next()
 })
-
-
-
 
 
 
@@ -97,6 +96,55 @@ app.get('/welcome', (req, res, next) => {
         username: req.cookies.username
     })
 })
+// ------------------------------------------------------------------------------------------------------------------------------
+
+// app.param() - takes 2 args
+//     1. param to look for in the route
+//     2. the callback to run (with the usuals)
+
+app.param('id', (req, res, next, id) => {
+
+    console.log('Params called: ', id);
+    // if id has something to do with stories...
+    // if id has something to do with blog...
+
+    next()
+})
+
+// --------------------- This code is ass but it works, instead we'll so something else
+// app.get('/story/1', (req, res, next) => {
+//     res.send(`<h1>Story 1<h1/>`)
+// })
+// app.get('/story/2', (req, res, next) => {
+//     res.send(`<h1>Story 2<h1/>`)
+// })
+// app.get('/story/3', (req, res, next) => {
+//     res.send(`<h1>Story 3<h1/>`)
+// })
+// ---------------------- ⬇️⬇️⬇️
+
+// in a route, anytime something has a ":" in front, is a WILDCARD!
+// WILDCARD's will match anything in that slot ⬇⬇
+
+app.get('/story/:storyId', (req, res, next) => {
+    // the req.params obj always exists
+    // it will have a property for each wildcard in the route
+    res.send(`<h1>Story ${req.params.storyId}</h1>`)
+    // res.send(`<h1>Story 1<h1/>`)
+})
+
+app.get('/story/:storyId/:link', (req, res, next) => {
+    // the req.params obj always exists
+    // it will have a property for each wildcard in the route
+    res.send(`<h1>Story ${req.params.storyId} - ${req.params.link}</h1>`)
+    // res.send(`<h1>Story 1<h1/>`)
+})
+// ------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
 
 app.get('/logout', (req, res, next) => {
     // res.clearCookie take 1 arg:
