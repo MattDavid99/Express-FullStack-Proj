59941b7cd5de1468ee8bdf3e5e15a9def8875d68
Matt <matthewdavid99@comcast.net>
2023-01-15 13:25:30 -0500

    index.js magic

--- movieApi/routes/index.js
+++ movieApi/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const movies = require('../data/movies')
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/most_popular', (req, res, next) => {
+  // get the page variable from the query string
+  let page = req.query.page;
+  if (page === undefined) page = 1;
+
+  if (req.query.api_key != 123456789) {
+    res.json("Invaild API Key")
+
+  } else {
+    let results = movies.filter((movie) => {
+      return movie.most_popular;
+    })
+    results = results.slice((page - 1) * 20, (page - 1) * 20 + 19)
+    res.json({ results })
+  }
+})
 
 module.exports = router;
--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 const request = require('request')
 
-const apiKey = `1fb720b97cc13e580c2c35e1138f90f8`;
-const apiBaseUrl = 'http://api.themoviedb.org/3';
-const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+// const apiKey = `1fb720b97cc13e580c2c35e1138f90f8`;
+const apiKey = `123456789`;
+// const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/most_popular?api_key=${apiKey}`;
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 router.use((req, res, next) => {
