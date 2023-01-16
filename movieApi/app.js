883371f0f22214d5b54dd15512705c00afaf5e57
Matt <matthewdavid99@comcast.net>
2023-01-16 14:07:47 -0500

    fullStack

--- movieApi/app.js
+++ movieApi/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// Changed dir to FullStackProj
 
 
 const helmet = require("helmet") // <<-- Remember to: npm install helmet --save
