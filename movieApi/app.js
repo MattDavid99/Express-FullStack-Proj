476cf86bfe753d0004465e2a8d1f8cf320f98b84
Matt <matthewdavid99@comcast.net>
2023-01-17 13:05:11 -0500

    search.js routes

--- movieApi/app.js
+++ movieApi/app.js
@@ -35,6 +35,8 @@ app.use((req, res, next) => {
   }
 })
 
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- movieApi/routes/search.js
+++ movieApi/routes/search.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET SEARCH page. */
-// /search/...
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
-});
+
+function queryRequired(req, res, next) {
+    const searchTerm = req.query.query
+    if (!searchTerm) {
+        res.json({
+            msg: "Query is required"
+        })
+
+    } else {
+        next()
+    }
+}
+
+// This middleware will be used by ALL routes in THIS router
+router.use(queryRequired)
+
+
+// GET /search/movie
+router.get('/movie', (req, res, next) => {
+    const searchTerm = req.query.query
+    if (!searchTerm) {
+        res.json({
+            msg: "Query is required"
+        })
+    }
+    res.json("Test")
+})
+
+
+
+// GET/search/person
+router.get('/person', (req, res, next) => {
+    res.json("Test")
+})
+
 
 module.exports = router;
